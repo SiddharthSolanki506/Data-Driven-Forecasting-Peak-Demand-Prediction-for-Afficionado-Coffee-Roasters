54fdec1fc9c8c019cf6b8180d11c89c7eeb6ec02
Siddharth Solanki <siddharthsolanki506@gmail.com>
2026-09-12 23:26:32 +0530

    Update 2_Model_performance.py

--- Afficionado_Coffee_Project/pages/2_Model_performance.py
+++ Afficionado_Coffee_Project/pages/2_Model_performance.py
@@ -21,8 +21,7 @@ st.divider()
 # Load Files
 # --------------------------------------
 
-BASE_DIR = Path(__file__).resolve().parent.parent
-
+Path(__file__).resolve().parent.parent
 importance_df = pd.read_csv(BASE_DIR / "feature_importance.csv")
 
 with open(BASE_DIR / "metrics.json", "r") as f:
